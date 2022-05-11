affecd1c11262df51ebf4db2365488c9e037176b
Dark-ManX <sergiosis@ukr.net>
2022-05-11 23:18:26 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchcountries';
 
 const DEBOUNCE_DELAY = 300;
+
+export const refs = {
+    input: document.querySelector('#search-box'),
+    country: document.querySelector('.country-list'),
+    countryContainer: document.querySelector('.country-info'),
+};
+
+refs.input.addEventListener('input', (e => {
+    const countryRef = e.target.value;
+
+    fetchCountries(countryRef);
+}));
+
