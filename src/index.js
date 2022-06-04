5b21887fffba18100ec4e12bd3879277962fc00d
Dark-ManX <sergiosis@ukr.net>
2022-06-05 00:15:36 +0300

    Revert "1"
    This reverts commit eac47b31d6121175bcdbf2c2f0c08f7238ab4909.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-$(".hero__img-box").owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
-  responsive: {
-    0: {
-      items: 1,
-    },
-    600: {
-      items: 3,
-    },
-    1000: {
-      items: 5,
-    },
-  },
-});
+import "./css/styles.css";
+import { fetchCountries } from "./js/fetchcountries";
+import obj from "./js/countrylist";
+import debounce from "lodash.debounce";
+// import Notiflix from 'notiflix';
+
+const DEBOUNCE_DELAY = 300;
+
+export const refs = {
+  input: document.querySelector("#search-box"),
+  country: document.querySelector(".country-list"),
+  countryContainer: document.querySelector(".country-info"),
+};
+
+refs.input.addEventListener(
+  "input",
+  debounce((e) => {
+    const countryRef = e.target.value.trim();
+
+    if (!countryRef) return;
+
+    fetchCountries(countryRef)
+      .then((country) => obj.fetchResult(country))
+      .catch((error) => obj.errorResult(error));
+  }, DEBOUNCE_DELAY)
+);
---
+++ src/js/fetchcountries.js
@@ -0,0 +1,10 @@
+import { refs } from "../index";
+
+export const fetchCountries = (name) => {
+  return fetch(`https://restcountries.com/v2/name/${name}`).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
